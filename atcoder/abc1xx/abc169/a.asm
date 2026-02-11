d220210a4991e22c0150d8bb282ffb207265f442
kento-yoshidu <kento_yoshidu@yahoo.co.jp>
2026-02-11 13:18:11 +0900

    🪱 ABC169 A

---
+++ atcoder/abc1xx/abc169/a.asm
@@ -0,0 +1,31 @@
+; https://atcoder.jp/contests/abc169/tasks/abc169_a
+
+section .data
+    fmt_in  db "%d %d", 0
+    fmt_out db "%d", 10, 0
+
+section .text
+    global main
+    extern scanf, printf
+
+main:
+    sub rsp, 24
+
+    lea rdi, [rel fmt_in]
+    lea rsi, [rsp]
+    lea rdx, [rsp + 4]
+    call scanf WRT ..plt
+
+    mov eax, [rsp]
+    imul eax, [rsp + 4]
+
+    lea rdi, [rel fmt_out]
+    mov esi, eax
+    xor eax, eax
+    call printf WRT ..plt
+
+    xor eax, eax
+    add rsp, 24
+    ret
+
+section .note.GNU-stack
