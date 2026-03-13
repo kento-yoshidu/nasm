c57b5babbe6ff8c0ab29ede064817733aa9b872d
kento-yoshidu <kento_yoshidu@yahoo.co.jp>
2026-03-14 08:57:36 +0900

    ☕ ma001

---
+++ atcoder/math_and_algorithm/001.asm
@@ -0,0 +1,30 @@
+; https://atcoder.jp/contests/math-and-algorithm/tasks/math_and_algorithm_a
+
+section .data
+    fmt_in  db "%d", 0
+    fmt_out db "%d", 10, 0
+
+section .text
+    global main
+    extern scanf, printf
+
+main:
+    sub rsp, 24
+
+    lea rdi, [rel fmt_in]
+    lea rsi, [rsp]
+    call scanf WRT ..plt
+
+    mov eax, [rsp]
+    add eax, 5
+
+    mov esi, eax
+    lea rdi, [rel fmt_out]
+    xor eax, eax
+    call printf WRT ..plt
+
+    xor eax, eax
+    add rsp, 24
+    ret
+
+section .note.GNU-stack
